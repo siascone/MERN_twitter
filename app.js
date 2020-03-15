fcd41e960d2c2b585104b1b7a39547a7e862c0a9
Spencer Iascone <spencer.iascone@gmail.com>
2020-03-15 16:46:27 -0700

    Set up debugging

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
