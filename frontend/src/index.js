bd113829577ddf504be9c95649726c80d0dfd5ec
Spencer Iascone <spencer.iascone@gmail.com>
2020-03-16 16:46:35 -0700

    Still hunting bugs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./components/root";
+import configureStore from "./store/store";
+import jwt_decode from "jwt-decode";
 
-import Root from './components/root';
-import configureStore from './store/store';
-import jwt_decode from 'jwt-decode';
-import {setAuthToken} from './util/session_api_util';
-import {logout} from './actions/session_actions';
+import { setAuthToken } from "./util/session_api_util";
+import { logout } from "./actions/session_actions";
 
-document.addEventListener('DOMContentLoaded', () => {
-    let store;
+document.addEventListener("DOMContentLoaded", () => {
+  let store;
 
-    if (localStorage.jwtToken) {
-        setAuthToken(localStorage.jwtToken);
-        const decodedUser = jwt_decode(localStorage.jwtToken);
-        const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
 
-        store = configureStore(preloadedState);
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = {
+      session: { isAuthenticated: true, user: decodedUser }
+    };
 
-        const currentTime = Date.now() / 1000;
+    store = configureStore(preloadedState);
 
-        if (decodedUser.exp < currentTime) {
-            store.dispatch(logout());
-            window.location.href = '/login';
-        }
-    } else {
-        store = configureStore({});
+    const currentTime = Date.now() / 1000;
+
+    if (decodedUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = "/login";
     }
-    
-    const root = document.getElementById('root');
+  } else {
+    store = configureStore({});
+  }
+  const root = document.getElementById("root");
 
-    ReactDOM.render(<Root store={store} />, root);
-})
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import Root from './components/root';
+// import configureStore from './store/store';
+// import jwt_decode from 'jwt-decode';
+// import {setAuthToken} from './util/session_api_util';
+// import {logout} from './actions/session_actions';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     let store;
+
+//     if (localStorage.jwtToken) {
+//         setAuthToken(localStorage.jwtToken);
+//         const decodedUser = jwt_decode(localStorage.jwtToken);
+//         const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+
+//         store = configureStore(preloadedState);
+
+//         const currentTime = Date.now() / 1000;
+
+//         if (decodedUser.exp < currentTime) {
+//             store.dispatch(logout());
+//             window.location.href = '/login';
+//         }
+//     } else {
+//         store = configureStore({});
+//     }
+    
+//     const root = document.getElementById('root');
+
+//     ReactDOM.render(<Root store={store} />, root);
+// })
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,13 +1,24 @@
-import { combineReducers } from 'redux';
-import session from './session_reducer';
-import errors from './errors_reducer';
-import tweets from './tweets_reducer';
+// import { combineReducers } from 'redux';
+// import session from './session_reducer';
+// import errors from './errors_reducer';
+// import tweets from './tweets_reducer';
 
 
+// const RootReducer = combineReducers({
+//     session,
+//     errors,
+//     tweets
+// });
+
+// export default RootReducer;
+
+import { combineReducers } from "redux";
+import session from "./session_reducer";
+import errors from "./errors_reducer";
+import tweets from "./tweets_reducer";
 const RootReducer = combineReducers({
-    session,
-    errors,
-    tweets
+  session,
+  errors,
+  tweets
 });
-
 export default RootReducer;
